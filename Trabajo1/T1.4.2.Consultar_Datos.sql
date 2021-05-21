78b2a12c05af0f95c18bc76b6e429ae6369a4dd7
jisazat <freddy.isaza@gmail.com>
2021-05-21 18:57:09 -0500

    Se agregan consultas adicionales de MongoDB

--- Trabajo1/T1.4.2.Consultar_Datos.sql
+++ Trabajo1/T1.4.2.Consultar_Datos.sql
@@ -1,5 +1,60 @@
+--Cantidad total de ordenes
+db.order.count()
+
+--Cantidad de ordenes por estado
 db.getCollection('order').aggregate(
     [ {$group : { _id : '$ORDERSTATUS', count : {$sum : 1}}} ]
 )
 
+--Vendedores con mayor número de ítems en ordenes
+db.getCollection('order').aggregate([
+  {$unwind: '$ORDERITEMS'},
+  {$group: {
+    _id: '$ORDERITEMS.SELLERID',
+    count: {$sum: 1}
+  }},
+  { $sort: { count: -1 } }
+])
 
+--Categorías de producto más vendidas
+db.getCollection('order').aggregate([
+  {$unwind: '$ORDERITEMS'},
+  {$group: {
+    _id: '$ORDERITEMS.PRODUCTS.CATEGORY',
+    count: {$sum: 1}
+  }},
+  { $sort: { count: -1 } }
+])
+
+-- Promedio de precio de venta 
+db.getCollection("order").aggregate([
+  { $unwind:  "$ORDERITEMS" },
+  { $unwind:  "$ORDERITEMS.PRODUCTS" },
+  { $group:{     _id:{  }, 
+             "count":{$sum:1},
+             "ORDERITEMS_PRICE-avg":{$avg:"$ORDERITEMS.PRICE"}  
+      }
+  },
+]);
+
+-- Precio máximo de venta 
+db.getCollection("order").aggregate([
+  { $unwind:  "$ORDERITEMS" },
+  { $unwind:  "$ORDERITEMS.PRODUCTS" },
+  { $group:{     _id:{  }, 
+             "count":{$sum:1},
+             "ORDERITEMS_PRICE-max":{$max:"$ORDERITEMS.PRICE"}  
+      }
+  },
+]);
+
+-- Precio mínimo de venta 
+db.getCollection("order").aggregate([
+  { $unwind:  "$ORDERITEMS" },
+  { $unwind:  "$ORDERITEMS.PRODUCTS" },
+  { $group:{     _id:{  }, 
+             "count":{$sum:1},
+             "ORDERITEMS_PRICE-min":{$min:"$ORDERITEMS.PRICE"}  
+      }
+  },
+]);
